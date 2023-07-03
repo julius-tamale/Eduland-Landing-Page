5db4c6bc47f12259d0743d75674ec16c13345413
julius-tamale <juliotamex@proton.me>
2023-07-03 11:31:55 +0300

    Eduland landing page

--- script.js
+++ script.js
@@ -7,14 +7,23 @@ const navOpenBtn = document.querySelector('.menu-open-btn'),
     navbar = document.querySelector('.navbar'),
     overlay = document.querySelector('.overlay');
 
+const headerEl = document.querySelector('.header');
 const navElemArr = [navOpenBtn, navCloseBtn, overlay]
 
+
+//hide or show nav menu on-click
 for(let navEl of navElemArr) {
     navEl.addEventListener('click', () => {
         overlay.classList.toggle('active');
         navbar.classList.toggle('active');
     })
 }
+
+//add the 'active' class to the header when scroll event occurs
+window.addEventListener('scroll', () => {
+    window.scrollY >= 20 ? headerEl.classList.add('active') 
+        : headerEl.classList.remove('active')
+})
 
 // console.log(navOpenBtn)
 // console.log(navCloseBtn)
